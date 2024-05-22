b1c4657231c23ce4843748dc05530bc441cfbc1b
Zike <sebastian160905@gmail.com>
2024-05-22 20:51:55 -0300

    Logico

---
+++ TecProg/Logico/Ej3.pl
@@ -0,0 +1,12 @@
+menu(['Bombones de jamon', 'Locro', 'Dulce de batata']).
+menu(['Bombones de jamon', 'Locro', 'Alfajor norteño']).
+menu(['Tarta de Atun', 'Atados de repollo', 'Dulce de batata']).
+menu(['Tarta de Atun', 'Pollo romano con hierbas y vino', 'Flan']).
+menu(['Volovanes de atun', 'Matambre con espinacas y parmesano', 'Torta moka']).
+menu(['Buñuelos de bacalao', 'Pollo romano con hierbas y vino', 'Alfajor norteño']).
+
+%a) menu([X,Y,Z])
+%b) menu([X,Y, 'Dulce de batata'])
+%c) menu([X,'Locro',Z])
+%d) menu([X,'Pato a la naranja',Z])
+%e) menu(['Locro',Y,Z])
