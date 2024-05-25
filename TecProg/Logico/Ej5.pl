41b2efa3baf979324ca728a4826089ca7b0ee88a
Zike420 <sebastian160905@gmail.com>
2024-05-25 17:08:43 -0300

    Logico

---
+++ TecProg/Logico/Ej5.pl
@@ -0,0 +1,13 @@
+ruta(santafe, parana).
+ruta(parana, corrientes).
+ruta(santafe, cordoba).
+ruta(santafe, coronda).
+ruta(santafe, rosario).
+ruta(rosario, capital).
+ruta(rosario, mardelplata).
+ruta(capital, cordoba).
+
+%a) ruta(X,cordoba) (santa fe y capital)
+%b) ruta(parana,Y) (corrientes)
+%c) ruta(parana,Y), ruta(Y,cordoba) (false)
+%d) ruta(santafe,Y), ruta(Y,X), ruta(X,corrientes) (false)
